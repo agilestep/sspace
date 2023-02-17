0f021a568c906cee8bd27db0b9605b7949b28217
agilestep <serghei.ocean@gmail.com>
2023-02-17 22:59:25 +0200

    Add some static articles on Ruby on Rails

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,19 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def why2023
+  end
+
+  def blocks_and
+  end
+
+  def tricks
+  end
+
+  def method_arguments
+  end
+
+  def regex
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'blogs/index'
+  get 'blogs/why2023'
+  get 'blogs/blocks_and'
+  get 'blogs/tricks'
+  get 'blogs/method_arguments'
+  get 'blogs/regex'
   get 'home/index'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
