0c1c90bf8f371f49b38c641d96a8e3ef558b6a7b
agilestep <serghei.ocean@gmail.com>
2023-03-03 01:38:46 +0200

    Add link_image helper

--- app/helpers/experience_helper.rb
+++ app/helpers/experience_helper.rb
@@ -1,2 +1,9 @@
 module ExperienceHelper
+
+  def link_image src:, width: '300', height: '300'
+    image_src = "/images/#{src}"
+    link_to(image_src, target: "_blank") do
+      image_tag(image_src,height: height, width: width)
+    end
+  end
 end
