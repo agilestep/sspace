bfec52d94acf8924e57c2fe96a04efc24cd3292e
agilestep <serghei.ocean@gmail.com>
2023-03-07 21:04:46 +0200

    Small home improvements

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+function closeStrTags(htmlString) {
+    const match = htmlString.match(/<(\w+)(?=\s|>|$)/g) || [];
+    const openTags = [];
+    match.forEach(tag => {
+        const tagName = tag.replace(/<(\w+).*/, '$1');
+        if (!tagName.startsWith('/')) {
+            openTags.push(tagName);
+        } else {
+            while (openTags.length && `/${openTags[openTags.length - 1]}` !== tagName) {
+                const closingTag = `</${openTags.pop()}>`;
+                htmlString += closingTag;
+            }
+            if (openTags.length) {
+                openTags.pop();
+            }
+        }
+    });
+    while (openTags.length) {
+        const closingTag = `</${openTags.pop()}>`;
+        htmlString += closingTag;
+    }
+    return htmlString;
+}
+
+
