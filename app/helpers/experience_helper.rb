c18d185888a6970fb0da01f3f10eef273be4bc2c
agilestep <serghei.ocean@gmail.com>
2023-03-07 23:17:33 +0200

    Mobile Friendly Experience Page

--- app/helpers/experience_helper.rb
+++ app/helpers/experience_helper.rb
@@ -1,9 +1,9 @@
 module ExperienceHelper
 
-  def link_image src:, width: '300', height: '300'
+  def link_image src:,class_name: ''
     image_src = "/images/#{src}"
     link_to(image_src, target: "_blank") do
-      image_tag(image_src,height: height, width: width)
+      image_tag(image_src,class: class_name)
     end
   end
 end
